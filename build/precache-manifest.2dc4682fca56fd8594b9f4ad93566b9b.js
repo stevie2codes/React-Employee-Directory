c1fcc3f464c3faf44656457c8c262ae185f8556d
stevie2codes <stevie2codes@gmail.com>
2020-03-20 13:23:00 -0500

    trying to deploy to github pages added gh-pages dependency

---
+++ build/precache-manifest.2dc4682fca56fd8594b9f4ad93566b9b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b33370978d8d5914e311fdc49c1a7e67",
+    "url": "/React-Employee-Directory/index.html"
+  },
+  {
+    "revision": "fdbb74e7bbf830c36d48",
+    "url": "/React-Employee-Directory/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "923a862a68f37fad3521",
+    "url": "/React-Employee-Directory/static/css/main.00f577de.chunk.css"
+  },
+  {
+    "revision": "fdbb74e7bbf830c36d48",
+    "url": "/React-Employee-Directory/static/js/2.9b3bde70.chunk.js"
+  },
+  {
+    "revision": "3b173ed738774b4bb64ac2b5ddb03f3b",
+    "url": "/React-Employee-Directory/static/js/2.9b3bde70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "923a862a68f37fad3521",
+    "url": "/React-Employee-Directory/static/js/main.edc41a5d.chunk.js"
+  },
+  {
+    "revision": "3107b6169e781a23dffc",
+    "url": "/React-Employee-Directory/static/js/runtime-main.4a1594cf.js"
+  }
+]);
